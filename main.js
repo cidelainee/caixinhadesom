302d57b5cb8f5ee8aca1db9a4bb87266459ee4d2
cidelainee <115091591+cidelainee@users.noreply.github.com>
2023-10-10 09:32:42 -0300

    Update main.js
    a

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
-alert("MENSAGEM");
 funcion tocaSomPom(){
 < document.querySelector('#som_tecla_pom'). play()
 }
-documet.querySelector('.tecla_pom').onclick=tocaSomPom;
+documet.querySelector('.tecla_pom').onclick=tocaSomPom;funcion tocaSomPom
+
+ funion tocaSomClap(){
+  < document.querySelector('#som_tecla_clap'). play()
+}
+documet.querySelector('.tecla_clap').onclick=tocaSomClap;
+
+
